57dbbd5019ae3d272e7a4c5aac77f2aaab0dd98b
meigarom <meigaromlopes@gmail.com>
2018-04-30 09:31:28 -0300

    add bash script to run bot

---
+++ run-bot.sh
@@ -0,0 +1,3 @@
+# Update techcrunch posts
+date >> /Users/meigarom/repos/webscraper-techcrunch/update_techcrunch.log
+cd /Users/meigarom/repos/webscraper-techcrunch && source .venv/bin/activate && python techcrunch-bot.py && deactivate
